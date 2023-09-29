7ae397acba1f7451d0cde3775ee863f6257e55ba
Bhavika Desai <patelbhavika846@gmail.com>
2023-09-29 16:08:40 +0530

    craete floid design

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+// Product Slider
+$('.our-best-row').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToShow: 4,
+  arrows: true,
+  variableWidth: true,
+  prevArrow: '<button type="button" class="slick-brand-prev"><img src="asset/images/arrow-slider.png"></button>',
+  nextArrow: '<button type="button" class="slick-brand-next"><img src="asset/images/arrow-slider.png"></button>',
+})
